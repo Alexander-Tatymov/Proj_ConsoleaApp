363316e89cfaa92ae1b9b1f431895e6d3838811c
Alexander Tatymov <ya.ya.wolf567@gmail.com>
2026-04-27 10:20:36 +0300

    feat: add AppLogger service for daily logs

---
+++ TaskTracker/TaskTracker.Storage/Services/AppLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Storage.Services;
+public class AppLogger
+{
+    private readonly string _logDir;
+    public AppLogger(string logDir)
+    {
+        _logDir = logDir;
+        Directory.CreateDirectory(_logDir);
+    }
+    private string GetLogFilePath()
+    {
+        // один файл на день
+        var fileName = $"app_{DateTime.Now:yyyy-MM-dd}.log";
+    
+    return Path.Combine(_logDir, fileName);
+    }
+    public void Info(string message)
+    {
+        var line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} INFO{ message}";
+    WriteLine(line);
+    }
+    public void Error(string message)
+    {
+        var line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} ERROR{ message}";
+    WriteLine(line);
+    }
+    private void WriteLine(string line)
+    {
+        var path = GetLogFilePath();
+        File.AppendAllText(path, line + Environment.NewLine);
+    }
+}
